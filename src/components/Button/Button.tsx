a329aad0518005f05d7bdb3fd59d50e117e5bea0
MakcChernenko <157323123+MakcChernenko@users.noreply.github.com>
2025-06-24 15:46:40 +0300

    made HW

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,19 +1,12 @@
-import css from "./button.module.css";
-import clsx from "clsx";
+// import css from './button.module.css';
+// import clsx from 'clsx';
 interface ButtonProps {
   name: number;
-  className?: "activ" | "notActiv";
-  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const Button = ({ name, className, onClick }: ButtonProps) => {
-  return (
-    <button
-      onClick={onClick}
-      className={clsx(css.button, className && css[className])}
-    >
-      {name}
-    </button>
-  );
+const Button = ({ name, onClick }: ButtonProps) => {
+  return <button onClick={onClick}> {name} </button>;
 };
+
 export default Button;
